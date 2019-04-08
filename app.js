f72e407eb18cdf5649f1d252506f873b0aa9f756
likejean <popachs@yahoo.com>
2019-04-08 10:14:26 -0700

    Cookies

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
+
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
@@ -31,34 +32,48 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth(req, res, next) {
-	console.log(req.headers);
+	console.log(req.signedCookies);
 
-	var authHeader = req.headers.authorization;
-	
-	if (!authHeader) {
-		var err = new Error('You are not authenthicated!');
+	if(!req.signedCookies.user) {
+		var authHeader = req.headers.authorization;
 
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		return next(err);
+		if (!authHeader) {
+			var err = new Error('You are not authenthicated!');
+
+			res.setHeader('WWW-Authenticate', 'Basic');
+			err.status = 401;
+			return next(err);
+		}
+
+		var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+		var username = auth[0];
+		var password = auth[1];
+
+		if (username === 'admin' && password === 'password') {
+			res.cookie('user', 'admin', { signed: true})
+			next();
+		}
+		else{
+			var err = new Error('You are not authenthicated, hacker!!');
+
+			res.setHeader('WWW-Authenticate', 'Basic');
+			err.status = 401;
+			return next(err);
+		}
 	}
+	else {
+		if (req.signedCookies.user === 'admin') {
+			next();
+		} else {
+			var err = new Error('You are not authenthicated, hacker!!');
 
-	var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-	var username = auth[0];
-	var password = auth[1];
-
-	if (username === 'admin' && password === 'password') {
-		next();
-	}
-	else{
-		var err = new Error('You are not authenthicated, hacker!!');
-
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		return next(err);
+			err.status = 401;
+			return next(err);
+		}
 	}
 }
 
