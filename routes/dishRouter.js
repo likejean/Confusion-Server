238ceb085d1bb52b2b704a538062d64b22d498e3
likejean <popachs@yahoo.com>
2019-04-12 13:35:18 -0700

    Improved Rest APIs

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -196,7 +196,7 @@ dishRouter.route('/:dishId/comments/:commentId')
     .then((dish) => {
         console.log("USER ID: ", req.user.id);
         console.log("AUTHOR ID: ", dish.comments.id(req.params.commentId).author);
-        if (req.user.id == (dish.comments.id(req.params.commentId).author) && !req.user.admin) {
+        if (req.user.id == (dish.comments.id(req.params.commentId).author)) {
             if (dish != null && dish.comments.id(req.params.commentId) != null) {
                 if (req.body.rating) {
                     dish.comments.id(req.params.commentId).rating = req.body.rating;
@@ -227,9 +227,11 @@ dishRouter.route('/:dishId/comments/:commentId')
             }
         } 
         else {
-            err = new Error('User ' + req.user.id + ' is not authorized to UPDATE this comment!!!');
-            err.status = 404;
-            return next(err);   
+            err = new Error('User ' + req.user.username + 
+            ' with ID: ' + req.user.id + 
+            ' is not authorized to CHANGE this comment!!!');
+        err.status = 404;
+        return next(err);     
         }       
     }, (err) => next(err))
     .catch((err) => next(err));
@@ -238,37 +240,48 @@ dishRouter.route('/:dishId/comments/:commentId')
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         console.log("USER ID: ", req.user.id);
-        console.log("AUTHOR ID: ", dish.comments.id(req.params.commentId).author);
-        if (req.user.id == (dish.comments.id(req.params.commentId).author) && !req.user.admin) {
-            if (dish != null && dish.comments.id(req.params.commentId) != null) {
-                dish.comments.id(req.params.commentId).remove();
-                dish.save()
-                .then((dish) => {
-                    Dishes.findById(dish._id)
-                    .populate('comments.author')
+        console.log("AUTHOR ID: ", dish.comments);
+        console.log("COMMENT: ", dish.comments.id(req.params.commentId));
+        if (dish.comments.id(req.params.commentId) != null){
+            if (req.user.id == (dish.comments.id(req.params.commentId).author)) {
+                if (dish != null && dish.comments.id(req.params.commentId).author != null) {
+                    dish.comments.id(req.params.commentId).remove();
+                    dish.save()
                     .then((dish) => {
-                        res.statusCode = 200;
-                        res.setHeader('Content-Type', 'application/json');
-                        res.json(dish);
-                    })                              
-                }, (err) => next(err));
-            }
-            else if (dish == null) {
-                err = new Error('Dish ' + req.params.dishId + ' not found');
-                err.status = 404;
-                return next(err);
+                        Dishes.findById(dish._id)
+                        .populate('comments.author')
+                        .then((dish) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(dish);
+                        })                              
+                    }, (err) => next(err));
+                }
+                else if (dish == null) {
+                    err = new Error('Dish ' + req.params.dishId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+                else {
+                    err = new Error('Comment ' + req.params.commentId + ' not found');
+                    err.status = 404;
+                    return next(err);            
+                }
             }
             else {
-                err = new Error('Comment ' + req.params.commentId + ' not found');
+                err = new Error('User ' + req.user.username + 
+                    ' with ID: ' + req.user.id + 
+                    ' is not authorized to DELETE this comment!!!');
                 err.status = 404;
-                return next(err);            
-            }
+                return next(err);   
+            }            
         }
         else {
-            err = new Error('User ' + req.user.id + ' is not authorized to DELETE this comment!!!');
+            err = new Error('This comment does not exist...');
             err.status = 404;
-            return next(err);   
+            return next(err); 
         }
+        
     }, (err) => next(err))
     .catch((err) => next(err));
 });
