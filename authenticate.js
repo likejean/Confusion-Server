537770f315b7c7e35720f1decf3c9332bbb7e35c
likejean <popachs@yahoo.com>
2019-04-15 10:38:52 -0700

    Passport-Facebook

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ var User = require('./models/users');
 var JwtStrategy = require('passport-jwt').Strategy //strategy for authenticating with a JSON Web Token
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 /// passport-jwt => A Passport strategy for authenticating with a JSON Web Token
 /// Passport is authentication middleware for Node, which serves one purpose, to authenticate requests
@@ -56,3 +57,32 @@ exports.verifyAdmin = function (req, res, next) {
     };
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+        clientID: config.facebook.clientId,
+        clientSecret: config.facebook.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({facebookId: profile.id}, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            if (!err && user != null) {
+                return done(null, user);
+            }
+            else {
+                user = new User({ username: profile.displayName });
+                user.facebookId = profile.id;
+                user.firstname = profile.name.givenName;
+                user.lastname = profile.name.familyName;
+                user.save((err, user) => {
+                    if (err){
+                       return done(err, false); 
+                    }
+                    else {
+                        return done(null, user);
+                    }                        
+                })
+            }
+        });
+    }
+));
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2307936442785857',
+        clientSecret: 'a2f99efc0c2576831b5c16fb81ee0bb5'
+    }
 }
